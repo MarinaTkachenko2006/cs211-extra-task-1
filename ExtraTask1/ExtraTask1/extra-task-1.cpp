578b508fcc7cafe0972b59a14f4507db065892a7
MarinaTkachenko2006 <tkachenkomari2006@gmail.com>
2024-10-02 16:00:10 +0300

    to_24_hour_clock added

--- ExtraTask1/ExtraTask1/extra-task-1.cpp
+++ ExtraTask1/ExtraTask1/extra-task-1.cpp
@@ -1,5 +1,6 @@
 #include "extra-task-1.h"
 #include <cassert>
+#include <cmath>
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
@@ -16,34 +17,12 @@ double to_float_hours(int hours, int minutes, int seconds)
     return hours + minutes / 60.0 + seconds / 3600.0;
 }
 
-//double to_24_hour_clock(double hours)
-//{
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
-//}
+double to_24_hour_clock(double hours)
+{
+    assert(hours >= 0);
+    int f = floor(hours);
+    return f % 24 + hours - f;
+}
 
 //int get_hours(int seconds)
 //{
--- ExtraTask1/ExtraTask1/main.cpp
+++ ExtraTask1/ExtraTask1/main.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 #include <cmath>
 #include <cfloat>
-#include <iostream>
 int main()
 {
     //assert ê ô-è seconds_difference()
@@ -22,4 +21,11 @@ int main()
     assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
     assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 
+    //assert ê ô-è to_24_hour_clock()
+    assert(fabs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(48) - 0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+    
 }
