d82c514fe05a9f75494811e7a9e6a47ae72de928
MarinaTkachenko2006 <tkachenkomari2006@gmail.com>
2024-10-02 15:43:57 +0300

    hours_difference added

--- ExtraTask1/ExtraTask1/extra-task-1.h
+++ ExtraTask1/ExtraTask1/extra-task-1.h
@@ -1,6 +1,8 @@
 #pragma once
+//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double, double);
 
+//Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
 double hours_difference(double, double);
 
 double to_float_hours(int, int, int);
